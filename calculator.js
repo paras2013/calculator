c97b0cb1b33bb8671f0c938adc6a08edb1a9b510
Twinkle Gandhi <twinklegandhi000@gmail.com>
2021-08-11 22:38:15 +0530

    calculator code using javascript

--- calculator.js
+++ calculator.js
@@ -1,12 +1,26 @@
+var num1;
+var num2;
+var ope;
+
 function ins(num) {
-    //
     document.getElementById("calc").value = document.getElementById("calc").value + num;
+    if(ope){
+        num2=document.getElementById("calc").value + num;
+    }
+    else{
+        num1=document.getElementById("calc").value + num;
+    }  
 }
 function equal() {
-    var eql = document.getElementById("calc").value;
+    console.log("num1  :"+num1+"   num2  : " +num2+"   ope  :  "+ope);
+    eql = document.getElementById("calc").value;
     if (eql) {
         document.getElementById("calc").value = eval(eql);
     }
+}
+function operator(ope1) {
+    ope=ope1;
+    console.log(ope);
 }
 function backspace() {
     var expression = document.getElementById("calc").value;
